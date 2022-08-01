808c9f1a747b8ddc1eeaa2c43a3fa4e377a15863
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-08-01 13:10:55 -0400

    updated https in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ searchCity.addEventListener("click", Citysearch);
 
 function Citysearch() {
    var input = city.value 
-    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
+    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
     fetch(url).then(function(res){
         return res.json()
     }).then(function(data){
@@ -18,7 +18,7 @@ function Citysearch() {
         }).then(function(data){
             console.log(data);
             document.querySelector(".cityName").innerText = input + " " + moment.unix(data.current.dt).format("MMMM Do YYYY");
-            document.querySelector(".icon").setAttribute("src", `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`);
+            document.querySelector(".icon").setAttribute("src", `https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`);
             document.querySelector(".temp").innerText = "Temp: " + data.current.temp + " F";
             document.querySelector(".wind").innerText = "Wind: " + data.current.wind_speed + " MPH";
             document.querySelector(".humidity").innerText = "Humidity: " + data.current.humidity + "%";
